2b8ff2b60a79325f7ee9d211e155fcdfb617ba81
rodcar <ivanrod12@hotmail.com>
2022-09-02 10:11:16 -0500

    eliminates unused imports

--- src/main/java/pe/rodcar/shelterpet/adoption/Application.java
+++ src/main/java/pe/rodcar/shelterpet/adoption/Application.java
@@ -2,9 +2,6 @@ package pe.rodcar.shelterpet.adoption;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 public class Application {
